9b48423829839938a5fae0556e015197e706b40b
kellim <kelli.blalock2@gmail.com>
2018-09-22 22:30:39 -0700

    Toggle deck body

--- js/app.js
+++ js/app.js
@@ -30,22 +30,44 @@ const deckSaver = {
   },
 
   createListItem: function(deck) {
-    const li = document.createElement('li');
+    const li  = document.createElement('li');
     const link = document.createElement('a');
     const linkText = document.createTextNode(deck.name);
-    console.log(link);
+
+    li.classList.add('deck-list-item');
     link.appendChild(linkText);
     link.setAttribute('href', '#');
     link.classList.add('deck-link', 'js-deck-link');
-    li.appendChild(link);     
+    link.setAttribute('aria-expanded', 'false');
+    
+    const span = document.createElement('span');
+    span.classList.add('expand-collapse-icon', 'js-expand-collapse-icon', 'fa', 'fa-caret-down');
+    link.appendChild(span);
+
+    const div = document.createElement('div');
+    const deckBodyId = deck.name.replace(/\s+/g, '-').toLowerCase() + '-body';
+    div.id = deckBodyId;
+    div.classList.add('deck-body', 'js-deck-body');
+    const bodyText = document.createTextNode(deck.body);
+    div.appendChild(bodyText);
+    // Associate id of element being collapsed with element that collapses them
+    // using aria-controls. Works on modern screen readers.
+    link.setAttribute('aria-controls', deckBodyId); 
+    
+    li.appendChild(link);
+    li.appendChild(div);
     this.deckListEl.appendChild(li);
   },
 
   addEventListeners: function() {
     this.deckListEl.addEventListener('click', e => {
       e.preventDefault();
-      if (e.target.tagName === 'A') {
-        console.log('Deck link was clicked');
+      console.log(e.target);
+      if (e.target.tagName === 'A' || e.target.tagName == 'SPAN') {
+        // If span is clicked on, get its parent a tag.
+        const linkEl = e.target.tagName === 'A' ? e.target : e.target.parentNode;
+        console.log('linkEl', linkEl)
+        this.toggleDeckBody(linkEl);
       }
     });
 
@@ -55,8 +77,26 @@ const deckSaver = {
     });
   },
 
+  toggleDeckBody: function(deckEl) {
+    const deckBodyEl = deckEl.parentNode.querySelector('.js-deck-body');
+    const linkEl = deckEl.parentNode.querySelector('.deck-link');
+    const expandCollapseIcon = linkEl.querySelector('.js-expand-collapse-icon');
+    
+    console.log('ecicon', expandCollapseIcon);
+    if (window.getComputedStyle(deckBodyEl).display === 'none') {
+      deckBodyEl.style.display = 'block';
+      expandCollapseIcon.classList.replace('fa-caret-down', 'fa-caret-up');
+      linkEl.setAttribute('aria-expanded', 'true');
+    } else {
+      deckBodyEl.style.display = 'none';
+      expandCollapseIcon.classList.replace('fa-caret-up', 'fa-caret-down');
+      linkEl.setAttribute('aria-expanded', 'false');
+    }
+  },
+
   processDeckForm: function() {
     // Decks copied from the game start with ### followed by a space, then the deck name.
+    // TODO: Make sure deck name is not the same as deck body ID - check for lowercase w/ dashes.
     if (this.deckInput.value.startsWith('### ')) {
       this.addDeck(this.deckInput.value);
       this.deckInput.value = '';
